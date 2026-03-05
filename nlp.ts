61f27918839f99cd70e1e4e0f80aa733f376b110
DanteDyk <dante.dykstrom@gmail.com>
2026-03-05 19:40:01 +0100

    Made search query ignore non-stop words inside quotation

--- nlp.ts
+++ nlp.ts
@@ -10,22 +10,32 @@ export function getTextTokens(text: string): string[] {
 
     const tokens = doc.tokens() //Get all text tokens
 
+    //Function that filters tokens a specified way, ignoring tokens inside quotation.
+    function ignoreQuotedTokens(token: SelectedTokens, func: Function): string[] {
+        const token_array = token.out(its.value);
+        const operated_tokens = token.out(func as ItsFunction<string>);
+        let insideQuotation = false;
+        for (let i = 0; i < token_array.length; i++) {
+            if (token_array[i] === '"') insideQuotation = !insideQuotation;
+            if (insideQuotation) operated_tokens[i] = token_array[i];
+        }
+        return operated_tokens;
+    }
+
     //Filter out all stop words, like "the", "is", "at"
     const filteredTokens = tokens.filter(token => !token.out(its.stopWordFlag));
 
-    function ignoreQuotedTokens(token: SelectedTokens): string {
-        let token_text = token.out(its.value);
-        let inside_quotation = false;
-        if (token_text === '"') inside_quotation = !inside_quotation;
-
-        return inside_quotation ? token_text : token.out(its.lemma as ItsFunction<string>);
-    }
+    
 
     //Filter out symbols https://winkjs.org/wink-nlp/part-of-speech.html
     const symbolFilteredTokens = filteredTokens.filter(token => token.out(its.pos) !== "SYM");
     console.log("Tokens with symbols removed: ", symbolFilteredTokens.out(its.value));
     //Lemmatize all words
-    const lemmatizedTokens = ignoreQuotedTokens(symbolFilteredTokens);
+
+    const lemmatizedTokens = ignoreQuotedTokens(symbolFilteredTokens, its.lemma);
+    console.log("function filter", ignoreQuotedTokens(symbolFilteredTokens, its.lemma));
+
+    
 
     //Filter out single character tokens
     const result = lemmatizedTokens.filter(token => token.length > 1);
