4058c841bbd25e1df8c7dec3adb21739267ec518
DanteDyk <dante.dykstrom@gmail.com>
2026-03-05 18:38:52 +0100

    Started work on refining search input treatment

--- nlp.ts
+++ nlp.ts
@@ -1,4 +1,4 @@
-import winkNLP, { ItsFunction } from 'wink-nlp';
+import winkNLP, { ItsFunction, ItemToken, SelectedTokens } from 'wink-nlp';
 import model from 'wink-eng-lite-web-model';
 
 const nlp = winkNLP(model);
@@ -13,11 +13,19 @@ export function getTextTokens(text: string): string[] {
     //Filter out all stop words, like "the", "is", "at"
     const filteredTokens = tokens.filter(token => !token.out(its.stopWordFlag));
 
+    function ignoreQuotedTokens(token: SelectedTokens): string {
+        let token_text = token.out(its.value);
+        let inside_quotation = false;
+        if (token_text === '"') inside_quotation = !inside_quotation;
+
+        return inside_quotation ? token_text : token.out(its.lemma as ItsFunction<string>);
+    }
+
     //Filter out symbols https://winkjs.org/wink-nlp/part-of-speech.html
     const symbolFilteredTokens = filteredTokens.filter(token => token.out(its.pos) !== "SYM");
-
+    console.log("Tokens with symbols removed: ", symbolFilteredTokens.out(its.value));
     //Lemmatize all words
-    const lemmatizedTokens = symbolFilteredTokens.out(its.lemma as ItsFunction<string>);
+    const lemmatizedTokens = ignoreQuotedTokens(symbolFilteredTokens);
 
     //Filter out single character tokens
     const result = lemmatizedTokens.filter(token => token.length > 1);
